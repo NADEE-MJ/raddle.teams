c6ad4dcb8050ea0b83ed489e78f1aefcfa087302
NADEE-MJ <nadeem.maida@gmail.com>
2025-08-30 19:35:38 -0700

    updates

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,16 +1,23 @@
 from datetime import datetime, timezone
 from typing import Optional
 
-from sqlalchemy import Column, ForeignKey as SA_ForeignKey
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey as SA_ForeignKey
 from sqlmodel import Field, SQLModel
+
+# TODO switch to using sqlmodel RELATIONSHIP
 
 
 class Player(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(unique=True)
     session_id: str = Field(unique=True)
-    lobby_id: int = Field(sa_column=Column(SA_ForeignKey("lobby.id", ondelete="CASCADE")))
-    team_id: Optional[int] = Field(default=None, sa_column=Column(SA_ForeignKey("team.id", ondelete="CASCADE")))
+    lobby_id: int = Field(
+        sa_column=Column(SA_ForeignKey("lobby.id", ondelete="CASCADE"))
+    )
+    team_id: Optional[int] = Field(
+        default=None, sa_column=Column(SA_ForeignKey("team.id", ondelete="CASCADE"))
+    )
     created_at: datetime = Field(default_factory=lambda: datetime.now(tz=timezone.utc))
 
 
@@ -18,7 +25,9 @@ class Team(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     # game_id: int = Field(foreign_key="game.id")
-    lobby_id: int = Field(sa_column=Column(SA_ForeignKey("lobby.id", ondelete="CASCADE")))
+    lobby_id: int = Field(
+        sa_column=Column(SA_ForeignKey("lobby.id", ondelete="CASCADE"))
+    )
     current_word_index: int = Field(default=0)
     completed_at: Optional[datetime] = Field(default=None)
     created_at: datetime = Field(default_factory=lambda: datetime.now(tz=timezone.utc))
@@ -43,7 +52,9 @@ class Lobby(SQLModel, table=True):
 class Guess(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     team_id: int = Field(sa_column=Column(SA_ForeignKey("team.id", ondelete="CASCADE")))
-    player_id: int = Field(sa_column=Column(SA_ForeignKey("player.id", ondelete="CASCADE")))
+    player_id: int = Field(
+        sa_column=Column(SA_ForeignKey("player.id", ondelete="CASCADE"))
+    )
     word_index: int
     direction: str  # "forward" or "backward"
     guess: str
